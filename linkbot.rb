293bcee4cae659ba7375d036d0f0960b9f3008f4
vanstee <vanstee@highgroove.com>
2011-08-15 23:32:10 -0400

    Write basic functionality

--- linkbot.rb
+++ linkbot.rb
@@ -1,11 +1,25 @@
 configure do
+  set :logging, false
+  enable :inline_templates
+  REDIS = Redis.new
+end
 
+helpers do
+  def add_link(link)
+    REDIS.rpush 'links', link
+  end
+  
+  def get_links
+    REDIS.lrange 'links', 0, -1
+  end
 end
 
 get '/' do
-  
+  @links = get_links
+  erb :links
 end
 
 post '/' do
-  
+  add_link(params[:link])
+  redirect '/'
 end
